8fa681e3183e196c0f3f2e777f2a3e0cb113bc7f
Ryan Smith <1578766+ryan3E0@users.noreply.github.com>
2020-08-15 10:59:47 -0400

    Update demo file and screenshots

---
+++ demo/demo.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function Example() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>You clicked {count} times.</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </>
+  );
+}
